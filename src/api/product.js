b898b5d1e4c5724f5cee6c119c973deb786e5100
cristianCor19 <cordobac96@gmail.com>
2025-01-28 17:16:35 -0500

    refactor: update registerProductRequest endpoint and improve error handling in ProductProvider

--- src/api/product.js
+++ src/api/product.js
@@ -2,8 +2,8 @@ import axios from './axios'
 
 export const getProductsRequest = async() => axios.get(`/product/` )
 
-export const registerProductRequest = (product) => axios.post(`/product/registerProduct`, product)
+export const registerProductRequest = (product) => axios.post(`/product/register-product`, product)
 
-// http://localhost:3000/product/registerProduct
+
 
 
--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,78 +1,80 @@
 import { createContext, useState, useContext, useEffect } from "react";
+import { enqueueSnackbar } from "notistack";
 
-import { registerProductRequest, getProductsRequest} from "../api/product";
+import { registerProductRequest, getProductsRequest } from "../api/product";
 
-
-export const ProductContext = createContext()
+export const ProductContext = createContext();
 
 //creacion de la funcion para poder entrar al contexto o las funciones de autenticacion
 export const useProduct = () => {
-    const context = useContext(ProductContext)
+  const context = useContext(ProductContext);
 
-    if (!context) {
-        throw new Error('useProduct debe utilizarse dentro de un Productprovider')
-    }
+  if (!context) {
+    throw new Error("useProduct debe utilizarse dentro de un Productprovider");
+  }
 
-    return context
-}
+  return context;
+};
 
 // eslint-disable-next-line react/prop-types
-export const ProductProvider = ({children}) => {
-    const [products, setProducts] = useState([])
-    const [errors, setErrors] = useState([])
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [errors, setErrors] = useState([]);
 
-
-    //funcion para obtener todos los productos
-    //function for get all products
-    const getProducts = async () => {
-        try {
-            const res = await getProductsRequest()
-            setProducts(res.data.data)
-            
-        } catch (error) {
-            console.log(error);
-        }
+  //funcion para obtener todos los productos
+  //function for get all products
+  const getProducts = async () => {
+    try {
+      const res = await getProductsRequest();
+      setProducts(res.data.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    //funcion para el registro de productos
-    //function for the register of products
-    const registerProduct = async (product) => {
-        try {
-            console.log('entro registro de producto');
-            const res = await registerProductRequest(product)
-         
-        } catch (error) {
-            setErrors(error.response.data)
-        }
+  //function to register the products
+  const registerProduct = async (product) => {
+    try {
+      const res = await registerProductRequest(product);
+      if (res.status === 200) {
+        enqueueSnackbar("Producto registrado con éxito ", {
+          variant: "success",
+          autoHideDuration: 3000,
+        });
+      }
+    } catch (error) {
+      setErrors(
+        Array.isArray(error.response.data)
+          ? error.response.data
+          : [error.response.data.message]
+      );
     }
+  };
 
+  //funcion paar quitar los mensajes de error, despues de cierto tiempo
 
-    //funcion paar quitar los mensajes de error, despues de cierto tiempo
+  useEffect(() => {
+    if (errors.length > 0) {
+      const timer = setTimeout(() => {
+        setErrors([]);
+      }, 5000);
 
-    useEffect(() => {
-        if(errors.length > 0){
-            const timer = setTimeout(() => {
-                setErrors([])
-            }, 5000)
+      return () => clearTimeout(timer);
+    }
+  }, [errors]);
 
-            return () => clearTimeout(timer)
-        }
-    }, [errors])
+  //funcion para realizar las verificaciones de seguridad respecto al manejo de tokens
 
-    //funcion para realizar las verificaciones de seguridad respecto al manejo de tokens
-   
-
-    return (
-        <ProductContext.Provider
-        value={{
-            products,
-            registerProduct,
-            getProducts,
-            errors
-            
-        }}
-        >
-            {children}
-        </ProductContext.Provider>
-    )
-}
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        registerProduct,
+        getProducts,
+        errors,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
