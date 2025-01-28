ee6aa7ef842505717232a944cb3fb3e3c2509d30
cristianCor19 <cordobac96@gmail.com>
2025-01-28 17:17:09 -0500

    feat: add totalSoldCategory and totalSoldMonth requests to Stats API and context

--- src/api/stats.js
+++ src/api/stats.js
@@ -2,3 +2,5 @@ import axios from "./axios";
 
 export const totalSoldRequest = async() => axios.get(`/stats/total-sold`)
 export const bestProductRequest = async() => axios.get(`/stats/best-selling-product`)
+export const totalSoldCategoryRequest = async() => axios.get(`/stats/total-sold-category`)
+export const totalSoldMonthRequest = async() => axios.get(`/stats/total-sold-month`)
